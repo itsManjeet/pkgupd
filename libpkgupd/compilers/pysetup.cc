a79bb6cf11f2e3d54b417b68ac70f00fa89f297f
itsmanjeet <itsmanjeet1998@gmail.com>
2022-03-06 00:34:20 +0530

    fixed pysetup tool

--- libpkgupd/compilers/pysetup.cc
+++ libpkgupd/compilers/pysetup.cc
@@ -7,14 +7,14 @@ namespace rlxos::libpkgupd {
 bool PySetup::compile(Recipe const& recipe, std::string dir,
                       std::string destdir, std::vector<std::string>& environ) {
   if (int status = Executor().execute(
-          "python pysetup.py build " + recipe.compile(), dir, environ);
+          "./setup.py build " + recipe.compile(), dir, environ);
       status != 0) {
     p_Error = "failed to build with python pysetup.py";
     return false;
   }
 
   if (int status = Executor().execute(
-          "python pysetup.py install --root='" + destdir + "' --optimize=1",
+          "./setup.py install --root='" + destdir + "' --optimize=1",
           dir, environ);
       status != 0) {
     p_Error = "failed to install with python pysetup.py";
