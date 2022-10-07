3d6b70aef88a13b2349c382d2b07a1d10d75185c
itsmanjeet <itsmanjeet1998@gmail.com>
2022-10-07 23:51:52 +0530

    pkgupd: added revdep option

--- pkgupd/pkgupd-revdep.cc
+++ pkgupd/pkgupd-revdep.cc
@@ -14,16 +14,20 @@ PKGUPD_MODULE_HELP(revdep) {
 
 PKGUPD_MODULE(revdep) {
   CHECK_ARGS(1);
-
-  std::string parent = args[0];
-
-  // auto repository = std::make_shared<Repository>(config);
-  auto system_database = SystemDatabase(config);
-  for (auto const& i : system_database.get()) {
-    if (std::find(i.second->depends().begin(), i.second->depends().end(),
-                  parent) != i.second->depends().end()) {
-      std::cout << "- " << i.second->id() << std::endl;
+  auto repository = Repository(config);
+  auto package = repository.get(args[0].c_str());
+  if (package == nullptr) {
+    ERROR("missing required package " << args[0]);
+    return 1;
+  }
+  for (auto const& i : repository.get()) {
+    if (find_if(i.second->depends().begin(), i.second->depends().end(),
+                [&](std::string id) -> bool {
+                  return id == package->id();
+                }) != i.second->depends().end()) {
+      std::cout << i.first << std::endl;
     }
   }
+
   return 0;
 }
