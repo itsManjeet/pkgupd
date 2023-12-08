ad04437141bb70cbba8cde47377d84a3a53d2fee
itsmanjeet <itsmanjeet1998@gmail.com>
2023-12-08 12:20:26 +0000

    engine: fixed installation data dump with integration

---
+++ src/Engine.cxx
@@ -0,0 +1,10 @@
+//
+// Created by itsmanjeet on 29/11/23.
+//
+
+#include "Engine.hxx"
+
+namespace rlxos {
+namespace libpkgupd {
+} // libpkgupd
+} // rlxos
---
+++ src/Engine.hxx
@@ -0,0 +1,18 @@
+//
+// Created by itsmanjeet on 29/11/23.
+//
+
+#ifndef ENGINE_HXX
+#define ENGINE_HXX
+
+namespace rlxos {
+namespace libpkgupd {
+
+class Engine {
+
+};
+
+} // libpkgupd
+} // rlxos
+
+#endif //ENGINE_HXX
